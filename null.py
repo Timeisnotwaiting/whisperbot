529469de1c81eeeb1b75a7e19054dad1b1a4b021
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-03 23:19:44 +0530

    Update null.py

--- null.py
+++ null.py
@@ -42,7 +42,7 @@ async def inline(event):
     me = (await null.get_me()).username
     try:
         inp = event.text.split(None, 1)[1]
-        user, msg = inp.split("")
+        user, msg = inp.split(" ")
     except IndexError:
         await event.answer(
                 [], 
