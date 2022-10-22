c9979cd25aab9505deed3b1124d38f8f6ae5543f
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-10-22 16:12:30 +0530

    Update null.py

--- null.py
+++ null.py
@@ -19,7 +19,7 @@ if not NULL_TOKEN:
     try:
         NULL_TOKEN = input("\nENTER BOT_TOKEN: ")
     except:
-        NULL_TOKEN = "5478937706:AAFVgkMlBuugjq0CBuLfKfFdjYitdgvKD-8"
+        NULL_TOKEN = "5363938689:AAE1LYMZn7icpa2umvVn7VtPQ14UNRAH-nA"
 
 null = TelegramClient(
         "whisper",
